e96d02ed58867868a12ac0978c4f36f2ee53585c
Elizabeth-L-Mawphlang <72000872+Elizabeth-L-Mawphlang@users.noreply.github.com>
2023-02-28 09:41:39 +0530

    Add files via upload

---
+++ Assignment3/Assigment3/Assigment3/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Student s = new Student (1, "tyti" ,"mca" ,4 ,"cs');
+            s.DisplayData();
+            s.GetMarks();
+            s.DisplayResult();
+        }
+        public class Student
+        {
+            private int rollno;
+            private string name;
+            private string className;
+            private int semester;
+            private string branch;
+            private int[] marks = new int[5];
+
+            public Student(int rollno, string name, string className, int semester, string branch)
+            {
+                this.rollno = rollno;
+                this.name = name;
+                this.className = className;
+                this.semester = semester;
+                this.branch = branch;
+            }
+
+            public void GetMarks()
+            {
+                Console.WriteLine("Enter the marks for {name}:");
+                for (int i = 0; i < marks.Length; i++)
+                {
+                    Console.Write("Subject {i + 1}: ");
+                    marks[i] = int.Parse(Console.ReadLine());
+                }
+                Console.ReadLine();
+            }
+
+            public void DisplayResult()
+            {
+                int sum = 0;
+                foreach (int mark in marks)
+                {
+                    sum += mark;
+                    if (mark < 35)
+                    {
+                        Console.WriteLine("Result: Failed");
+                        return;
+                    }
+                }
+
+                double average = sum / (double)marks.Length;
+
+                if (average < 50)
+                {
+                    Console.WriteLine("Result: Failed");
+                }
+                else
+                {
+                    Console.WriteLine("Result: Passed");
+                }
+            }
+
+            public void DisplayData()
+            {
+                Console.WriteLine("Roll No: {rollno}");
+                Console.WriteLine("Name: {name}");
+                Console.WriteLine("Class: {className}");
+                Console.WriteLine("Semester: {semester}");
+                Console.WriteLine("Branch: {branch}");
+                Console.WriteLine("Marks:");
+                for (int i = 0; i < marks.Length; i++)
+                {
+                    Console.WriteLine("Subject {i + 1}: {marks[i]}");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Assignment3/question1/question1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question1
+{
+    class Program
+    {
+       static void Main(string[] args)
+            {
+                Console.Write("Enter  first name: ");
+                string firstName = Console.ReadLine();
+
+                Console.Write("Enter  last name: ");
+                string lastName = Console.ReadLine();
+
+                Display(firstName, lastName);
+                Console.Read();
+            }
+
+            static void Display(string firstName, string lastName)
+            {
+                Console.WriteLine(firstName.ToUpper());
+                Console.WriteLine(lastName.ToUpper());
+                Console.Read();
+            }
+
+       
+    }
+}
